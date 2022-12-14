fa6fa8e0bea9d41fe09d02ed9bf4ba13edba2675
jim-low <robil12380@gmail.com>
2022-12-14 23:36:53 +0800

    chore: change script name from PlayerScriptForTesting to Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerScriptForTesting : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private float health = 100f;
 
