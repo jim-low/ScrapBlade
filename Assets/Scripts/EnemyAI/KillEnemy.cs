613839483333f4c08eb752276a31d2891ffbdc54
jim-low <robil12380@gmail.com>
2022-12-17 12:03:32 +0800

    feat: disable animator to show ragdoll effect of enemy death

--- Assets/Scripts/EnemyAI/KillEnemy.cs
+++ Assets/Scripts/EnemyAI/KillEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class KillEnemy : MonoBehaviour
 {
     void OnTriggerEnter(Collider collider)
@@ -9,6 +10,7 @@ public class KillEnemy : MonoBehaviour
         if (collider.gameObject.tag == "Sword")
         {
             Debug.Log(gameObject.name + " has been unalived");
+            GetComponent<Animator>().enabled = false;
         }
     }
 }
