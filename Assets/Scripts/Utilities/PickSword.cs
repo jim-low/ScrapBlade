ffb7c213d8193c2df87924cad3af6f1cd24bdd91
jim-low <robil12380@gmail.com>
2022-12-18 02:11:14 +0800

    feat: slowly decrease light intensity until it shuts off

--- Assets/Scripts/Utilities/PickSword.cs
+++ Assets/Scripts/Utilities/PickSword.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class PickSword : Interactable
 {
 	private bool hasTakenSword = false;
+	private Light spotlight;
 	public Transform spawnPoint;
 	public GameObject swordPrefab;
 
 	void Start()
 	{
 		hint = "Take Sword";
+		spotlight = GetComponentInChildren<Light>();
+	}
+
+	void Update()
+	{
+		if (hasTakenSword)
+		{
+			spotlight.intensity = spotlight.intensity - 0.5f;
+		}
 	}
 
 	protected override void Action()
@@ -18,10 +28,14 @@ public class PickSword : Interactable
 		if (hasTakenSword)
 			return;
 
+		// Destroy pivot point
 		Destroy(GameObject.Find("SwordRotationPivot"));
 
+		// instantiate sword in player hand
 		GameObject swordInstance = Instantiate(swordPrefab, spawnPoint);
 		swordInstance.name = "PlayerSword";
+
+		// setting flags
 		hasTakenSword = true;
 		SwordAttack.isPickedUp = true;
 		hint = "";
