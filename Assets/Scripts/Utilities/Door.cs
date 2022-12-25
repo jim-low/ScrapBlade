d61a068d73e8b7c246f0aede0454911a29a063fa
jim-low <robil12380@gmail.com>
2022-12-25 22:25:19 +0800

    feat: add opening and closing sounds to door

--- Assets/Scripts/Utilities/Door.cs
+++ Assets/Scripts/Utilities/Door.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Door : Interactable
 {
 	private bool isOpen = false;
+	private AudioSource source;
+	public AudioClip doorOpen;
+	public AudioClip doorClose;
 	private Animator anim;
 
 	void Start()
 	{
 		anim = transform.parent.GetComponent<Animator>();
+		source = transform.parent.GetComponent<AudioSource>();
 		hint = "Open Door";
 	}
 
@@ -20,6 +24,8 @@ public class Door : Interactable
 
 		isOpen = true;
 		anim.SetBool("character_nearby", true);
+		source.clip = doorOpen;
+		source.Play();
 		hint = "";
 		StartCoroutine(CloseDoor());
 	}
@@ -29,6 +35,8 @@ public class Door : Interactable
 		yield return new WaitForSeconds(2f);
 		isOpen = false;
 		anim.SetBool("character_nearby", false);
+		source.clip = doorClose;
+		source.Play();
 		hint = "Open Door";
 	}
 }
