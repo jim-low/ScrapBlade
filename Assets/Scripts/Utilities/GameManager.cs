a208c22ee62f589caa29ecdf6ce38bf3f9c1b5e9
jim-low <robil12380@gmail.com>
2022-12-25 21:31:34 +0800

    feat: add button click sound

--- Assets/Scripts/Utilities/GameManager.cs
+++ Assets/Scripts/Utilities/GameManager.cs
@@ -8,12 +8,23 @@ using UnityEngine.SceneManagement;
 // Go to main menu (we dont have it yet)
 // Go to settings page (we dont have it yet)
 
+[RequireComponent(typeof(AudioSource))]
 public class GameManager : MonoBehaviour
 {
+    private AudioSource buttonSource;
+    public AudioClip buttonClick;
+
+    void Start()
+    {
+        buttonSource = GetComponent<AudioSource>();
+        buttonSource.clip = buttonClick;
+    }
+
     public void Restart()
     {
         Player.isDied = false;
         GameObject.Find("PlayerUI").transform.Find("Buttons").gameObject.SetActive(false);
+        buttonSource.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
@@ -21,10 +32,12 @@ public class GameManager : MonoBehaviour
     {
 
         Debug.Log("currently this does nothing");
+        buttonSource.Play();
     }
 
     public void SettingPage()
     {
         Debug.Log("currently this does nothing");
+        buttonSource.Play();
     }
 }
