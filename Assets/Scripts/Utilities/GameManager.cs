3a393f792331b7a20308a2b887bba67c03145aeb
jim-low <robil12380@gmail.com>
2022-12-26 19:59:27 +0800

    feat: add escape menu

--- Assets/Scripts/Utilities/GameManager.cs
+++ Assets/Scripts/Utilities/GameManager.cs
@@ -8,17 +8,53 @@ using UnityEngine.SceneManagement;
 // Go to main menu
 // Go to settings page
 // Play level 1
+// Pause Level
+// Resume Level
 
 [RequireComponent(typeof(AudioSource))]
 public class GameManager : MonoBehaviour
 {
     private AudioSource buttonSource;
     public AudioClip buttonClick;
+    public GameObject inGameMenu;
+    private bool paused = false;
 
     void Start()
     {
         buttonSource = GetComponent<AudioSource>();
         buttonSource.clip = buttonClick;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            if (paused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        GameObject.Find("BackgroundMusic").GetComponent<AudioSource>().Pause(); // stop music
+        Time.timeScale = 0; // stop gameplay
+		Cursor.lockState = CursorLockMode.None; // release mouse lock
+        inGameMenu.SetActive(true); // show pause menu
+    }
+
+    public void ResumeGame()
+    {
+        GameObject.Find("BackgroundMusic").GetComponent<AudioSource>().UnPause(); // continue music
+        Time.timeScale = 1; // continue gameplay
+		Cursor.lockState = CursorLockMode.Locked; // lock mouse
+        inGameMenu.SetActive(false); // hide pause menu
     }
 
     public void Restart()
