1410ddf2e7e9e0db48501dd0e21184cd8fa921af
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2022-12-30 00:25:41 +0800

    Finished Timer
    yey, although have not tested with multiple retries

--- Assets/Scripts/Utilities/HighScoreTimer.cs
+++ Assets/Scripts/Utilities/HighScoreTimer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HighScoreTimer : MonoBehaviour
 {
@@ -10,28 +11,39 @@ public class HighScoreTimer : MonoBehaviour
     static float deltaSeconds = 0;
     static int secondsPassed = 0;
     static int minutesPassed = 0;
-    static string finalTime;
+    static string finalHighestTime = "99:99";
     static string onCountTime;
-    static int highestTime = 0;
+    static int highestTime = 1000000;
 
     //string variables
     string substituteZero = "0";
     string colon = ":";
-    string minutes = "minutes ";
-    string seconds = "seconds ";
+    string minutes = " minutes ";
+    string seconds = " seconds ";
+    string empty = "";
 
     //indication of the end of game
     static bool gameEnd = false;
 
     //object grabbing
     GameObject timeText;
+    GameObject displayHighScore;
+
+    //varibles to stop the timer and process the highest score 
+    string curScene;
+    string creditScene = "CreditScene";
+    string levelOne = "Level1";
+    string fastestTime = "Fastest Time\n";
+    string yourTime = "Your Time\n";
 
     // Start is called before the first frame update
     void Start()
     {
         //get the text object
         timeText = GameObject.Find("PlayerUI").transform.Find("Timer").gameObject;
+        displayHighScore = GameObject.Find("HighScoreBoard").transform.Find("Canvas5").Find("Text5").gameObject;
 
+        curScene = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
@@ -43,23 +55,17 @@ public class HighScoreTimer : MonoBehaviour
             secondsPassed -= 60; //remove count
         }
 
-        //if the game ends, stop counting the timer, display in the credits scene
-        if (gameEnd)
-        {
-            //check if the time is faster, overwrite if it is
-            if ((secondsPassed + (minutesPassed * 60)) < highestTime) {
-                highestTime = (secondsPassed + (minutesPassed * 60));
-                //format and convert to string
-                finalTime = minutesPassed.ToString() + minutes + secondsPassed.ToString() + seconds;
-            }
+        //if it is at the credits Scene, stop the timer.
+        if (curScene == creditScene) {
+            gameEnd = true;
         }
 
-        else 
-        { 
-            //count and convert to seconds and minutes passed. 
-            deltaSeconds += Time.deltaTime;
-            secondsPassed = Mathf.FloorToInt(deltaSeconds % 60); //secondsPassed will update in seconds. 
-            minutesPassed = Mathf.FloorToInt(deltaSeconds / 60); //minutesPassed will update in minutes. 
+        if (curScene == levelOne) {
+            gameEnd = false;
+            deltaSeconds = 0;
+            secondsPassed = 0;
+            minutesPassed = 0;
+            onCountTime = substituteZero + substituteZero + colon + substituteZero + substituteZero;
         }
 
         //format to string and display the time.
@@ -89,8 +95,36 @@ public class HighScoreTimer : MonoBehaviour
 
         }
 
-        // display the time
-        timeText.GetComponent<UnityEngine.UI.Text>().text = onCountTime;
-        
+        //if the game ends, stop counting the timer, display in the credits scene
+        if (gameEnd)
+        {
+            //check if the time is faster, overwrite if it is
+            if ((secondsPassed + (minutesPassed * 60)) < highestTime)
+            {
+                highestTime = (secondsPassed + (minutesPassed * 60));
+                //format and convert to string
+                finalHighestTime = minutesPassed.ToString() + minutes + secondsPassed.ToString() + seconds;
+            }
+
+            //get the text and display
+            displayHighScore.GetComponent<UnityEngine.UI.Text>().text = fastestTime + finalHighestTime + yourTime + onCountTime;
+
+            //remove the text on upper left
+            timeText.GetComponent<UnityEngine.UI.Text>().text = empty;
+        }
+
+        else
+        {
+            //count and convert to seconds and minutes passed. 
+            deltaSeconds += Time.deltaTime;
+            secondsPassed = Mathf.FloorToInt(deltaSeconds % 60); //secondsPassed will update in seconds. 
+            minutesPassed = Mathf.FloorToInt(deltaSeconds / 60); //minutesPassed will update in minutes. 
+
+            // display the time
+            timeText.GetComponent<UnityEngine.UI.Text>().text = onCountTime;
+        }
+
+
+
     }
 }
