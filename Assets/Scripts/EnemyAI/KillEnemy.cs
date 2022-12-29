ade296069db8996c4880d7c0325598e1cbbd9314
Brian <111887232+talon-2@users.noreply.github.com>
2022-12-29 13:37:29 +0800

    Enemy Death Particle

--- Assets/Scripts/EnemyAI/KillEnemy.cs
+++ Assets/Scripts/EnemyAI/KillEnemy.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+
+
 public class KillEnemy : MonoBehaviour
 {
+    [Header("Death Particle")]
+    public GameObject deathSpark;
+    private string sparkName;
+
+    void Start()
+    {
+        sparkName = "DeathSpark";
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Sword" && Sword.isAttacking)
         {
             GetComponent<Animator>().enabled = false;
             GetComponent<RangedEnemy>().SetLiveStatus(false);
+            GameObject temp = Instantiate(deathSpark, transform.position, Quaternion.identity);
+            temp.name = sparkName;
         }
     }
 }
